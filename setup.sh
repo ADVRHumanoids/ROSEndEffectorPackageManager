0e0df9e07d99f9dd5b256dc3211df5ae4c8c3343
torydebra <toridebraus@gmail.com>
2020-07-21 15:23:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo apt-get install ros-kinetic-moveit
+
+#I do not know if these two are the same...
+sudo apt install ros-kinetic-ros-control
 sudo apt install ros-kinetic-control-toolbox
