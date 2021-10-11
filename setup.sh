18e697adadfaefb58284a6cab5f89c7d68ea9383
torydebra <toridebraus@gmail.com>
2021-10-11 12:19:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ sudo apt-get install ros-$ROS_DISTRO-moveit
 #I do not know if these two are the same...
 sudo apt install ros-$ROS_DISTRO-ros-control
 sudo apt install ros-$ROS_DISTRO-control-toolbox
+
+#python numexpr necessary for the joint state publisher mimic nonlinear fork
+sudo apt install python-numexpr #or sudo apt install python3-numexpr
